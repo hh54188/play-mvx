9a564c73920dea7f742c38a66ca2d3897107db0c
invegas <juststayinvegas@gmail.com>
2014-01-02 00:09:13 +0800

    update

--- backbone/router.js
+++ backbone/router.js
@@ -4,37 +4,35 @@ var Router = Backbone.Router.extend({
         "save/:id": "save",
         "delete/:id": "delete",
         "edit/:id": "edit",
+        "check/:id": "check",
         "uncheck/:id": "uncheck",
         // filter
         "filter/completed": "filterByCompleted",
         "filter/uncompleted": "filterByUncompleted"
+    },
+    save: function () {
+       alert("SAVE!"); 
+    },
+    delete: function () {
+
+    },
+    edit: function () {
+
+    },
+    check: function () {
+
+    },
+    uncheck: function () {
+
+    },
+    filterByCompleted: function () {
+
+    },
+    filterByUncompleted: function () {
+        
     }
 })
 
 var router = new Router();
-
-router.on("route:delete", function (id) {
-    if (!id) return;
-});
-
-router.on("route:edit", function (id) {
-    if (!id) return;
-});
-
-router.on("route:save", function () {
-    alert("SAVE!");
-});
-
-router.on("route:uncheck", function (id) {
-    if (!id) return;
-});
-
-router.on("route:filterByCompleted", function () {
-
-});
-
-router.on("route:filterByUncompleted", function () {
-
-});
 
 Backbone.history.start();
