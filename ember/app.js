5d9fda470190488f878433a2501cc519f302314e
hh54188 <252438823@qq.com>
2014-01-07 15:24:54 +0800

    ember complete

--- ember/app.js
+++ ember/app.js
@@ -1,13 +1,46 @@
 App = Ember.Application.create({});
 
-App.ApplicationAdapter = DS.FixtureAdapter;
+// App.ApplicationAdapter = DS.FixtureAdapter;
+
+// App.Todo = Ember.Object.extend({
+//     title: ""
+// });
+
+App.ApplicationAdapter = DS.LSAdapter.extend();
 
 App.Todo = DS.Model.extend({
-    title: DS.attr()
+	title: DS.attr("string")
 })
 
 App.IndexRoute = Ember.Route.extend({
     setupController: function(controller) {
-        controller.set('content', ['red', 'yellow', 'blue']);
+    	var todos = this.store.find('todo');
+    	controller.set("content", todos);
     }
 });
+
+App.TodoController = Ember.ObjectController.extend({
+	actions: {
+		remove: function () {
+			var todo = this.get('model');
+
+			todo.deleteRecord();
+			todo.save();
+		}
+	}
+})
+
+App.IndexController = Ember.ArrayController.extend({
+
+	actions: {
+		create: function () {
+			var title = this.get("title");
+
+			var todo = this.store.createRecord('todo', {
+				title: title
+			});
+
+			todo.save();
+		}
+	}
+})
